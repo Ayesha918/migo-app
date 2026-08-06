3111e503666e3a56866052b27b5469be970c855d
Ayesha918 <myayesha17@gmail.com>
2026-08-06 07:05:00 +0530

    Point frontend api services to live production backend server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://YOUR_LIVE_BACKEND_URL.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
